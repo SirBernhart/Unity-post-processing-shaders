93ab5cca6b3feb099524b8619138da8fcb3e7ad1
Bernardo Hörner <bernardo.horner@tapps.com.br>
2022-06-07 17:01:58 -0300

    Adjust toon shader to have more configurable parameters

--- Generic Shaders/Assets/Generic shaders/_Project/Shaders/Toon/ToonPostProcessing.cs
+++ Generic Shaders/Assets/Generic shaders/_Project/Shaders/Toon/ToonPostProcessing.cs
@@ -8,12 +8,70 @@ public sealed class ToonPostProcessing : PostProcessEffectSettings
 {
     [Range(0f, 1f)]
     public FloatParameter shadowBrightness = new FloatParameter {value = 0.5f};
-    [Range(0f, 1f)]
+    [Range(0f, 2f)]
     public FloatParameter maxBrightness = new FloatParameter {value = 0.75f};
     [Range(0f, 1f)]
     public FloatParameter transitionSmoothness = new FloatParameter {value = 0.5f};
     [Range(0f, 1f)]
     public FloatParameter maxShadedValue = new FloatParameter {value = 0.75f};
+    [Range(2f, 5f)]
+    public IntParameter steps = new IntParameter {value = 3};
+    [Range(0f, 1f)]
+    public FloatParameter band1Brightness = new FloatParameter() {value = 0.2f};
+    [Range(0f, 1f)]
+    public FloatParameter band2Brightness = new FloatParameter() {value = 0.4f};
+    [Range(0f, 1f)]
+    public FloatParameter band3Brightness = new FloatParameter() {value = 0.6f};
+    [Range(0f, 1f)]
+    public FloatParameter band4Brightness = new FloatParameter() {value = 0.8f};
+    [Range(0f, 1f)]
+    public FloatParameter band5Brightness = new FloatParameter() {value = 1.0f};
+    [Range(0f,1f)]
+    public FloatParameter edge1 = new FloatParameter() {value = 0.2f};
+    [Range(0f,1f)]
+    public FloatParameter edge2 = new FloatParameter() {value = 0.4f};
+    [Range(0f,1f)]
+    public FloatParameter edge3 = new FloatParameter() {value = 0.6f};
+    [Range(0f,1f)]
+    public FloatParameter edge4 = new FloatParameter() {value = 0.8f};
+
+    private int lastSetSteps = 0;
+    private void OnValidate()
+    {
+        if (lastSetSteps == steps)
+        {
+            return;
+        }
+
+        lastSetSteps = steps;
+        float stepInterval = 1.0f / steps;
+        float currentStep = stepInterval;
+        for (int i = 0; i < steps; i++, currentStep += stepInterval)
+        {
+            switch (i)
+            {
+                case 0:
+                    band1Brightness.value = currentStep;
+                    edge1.value = currentStep;
+                    continue;
+                case 1:
+                    band2Brightness.value = currentStep;
+                    edge2.value = currentStep;
+                    continue;
+                case 2:
+                    band3Brightness.value = currentStep;
+                    edge3.value = currentStep;
+                    continue;
+                case 3:
+                    band4Brightness.value = currentStep;
+                    edge4.value = currentStep;
+                    continue;
+                case 4:
+                    band5Brightness.value = currentStep;
+                    continue;
+            }
+        }
+    }
 }
 
 public sealed class ToonPostProcessingRenderer : PostProcessEffectRenderer<ToonPostProcessing>
@@ -25,6 +83,27 @@ public sealed class ToonPostProcessingRenderer : PostProcessEffectRenderer<ToonP
         sheet.properties.SetFloat("_MaxBrightness", settings.maxBrightness);
         sheet.properties.SetFloat("_TransitionSmoothness", settings.transitionSmoothness);
         sheet.properties.SetFloat("_MaxShadedValue", settings.maxShadedValue);
+        sheet.properties.SetInt("_NumberOfSteps", settings.steps);
+
+        float[] bandBrightnessArray =
+        {
+            settings.band1Brightness,
+            settings.band2Brightness,
+            settings.band3Brightness,
+            settings.band4Brightness,
+            settings.band5Brightness,
+        };
+
+        float[] edgesArray =
+        {
+            settings.edge1,
+            settings.edge2,
+            settings.edge3,
+            settings.edge4
+        };
+
+        sheet.properties.SetFloatArray("_BandBrightnessArray", bandBrightnessArray);
+        sheet.properties.SetFloatArray("_EdgesArray", edgesArray);
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
 }
--- Generic Shaders/Assets/Generic shaders/_Project/Shaders/Toon/ToonPostProcessing.shader
+++ Generic Shaders/Assets/Generic shaders/_Project/Shaders/Toon/ToonPostProcessing.shader
@@ -9,7 +9,9 @@ Shader "Hidden/Bernardo/ToonPostProcessing"
     float _TransitionSmoothness;
     float _MaxShadedValue;
     float _MaxBrightness;
-
+    int _NumberOfSteps;
+    float _BandBrightnessArray[5];
+    float _EdgesArray[4];
 
     // Hue, Saturation, Value
     // Ranges:
@@ -33,14 +35,44 @@ Shader "Hidden/Bernardo/ToonPostProcessing"
         return c.z * lerp(K.xxx, saturate(p - K.xxx), c.y);
     }
 
+    float Posterize(float In, float steps)
+    {
+        return floor(In * steps/_MaxBrightness) * _MaxBrightness/steps;
+    }
+
     float4 Frag(VaryingsDefault i) : SV_Target
     {
         float3 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord).xyz;
         float3 hsvColor = RgbToHsv(color);
 
-        float upperEdge = _MaxShadedValue + _TransitionSmoothness;
+        float newValue = _BandBrightnessArray[_NumberOfSteps - 1];
+
+        for(int index = 0 ; index < _NumberOfSteps-1 ; index++)
+        {
+            float currEdge = _EdgesArray[index];
+            float nextEdge = _EdgesArray[index + 1];
+            if(hsvColor.z < (currEdge + nextEdge)/2)
+            {
+                newValue = _BandBrightnessArray[index] +
+                    smoothstep(currEdge - _TransitionSmoothness, currEdge + _TransitionSmoothness, hsvColor.z)
+                    * (_BandBrightnessArray[index+1] - _BandBrightnessArray[index]);
+                break;
+            }
+        }
+
+        hsvColor.z = newValue;
+
+        //float lowerEdge = GetLowerEdge(hsvColor.z, _NumberOfSteps);
+        //hsvColor.z = Posterize(hsvColor.z, _NumberOfSteps);
+        /*hsvColor.z = min(hsvColor.z, _MaxBrightness);
+        hsvColor.z = max(hsvColor.z, _ShadowBrightness);*/
+        /*float upperEdge = lowerEdge + _TransitionSmoothness;
+        hsvColor.z = smoothstep(lowerEdge, upperEdge, hsvColor.z);
+        hsvColor.z = min(hsvColor.z, _MaxBrightness);*/
+
+        /*float upperEdge = _MaxShadedValue + _TransitionSmoothness;
         hsvColor.z = _ShadowBrightness + smoothstep(_MaxShadedValue, upperEdge, hsvColor.z) * (1.0 - _ShadowBrightness);
-        hsvColor.z = min(hsvColor.z, _MaxBrightness);
+        hsvColor.z = min(hsvColor.z, _MaxBrightness);*/
 
         color = HsvToRgb(hsvColor);
 
